1a80516a53b75bb9e31cca9e3bb5f63ef8c3e335
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2017-03-02 11:07:01 +0100

    Enma baseline

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 mkdir build/files -p
 cd build/files 
 wget -O- get.pharo.org/50+vm | bash
-./pharo Pharo eval --save " Metacello new baseline: 'Fog'; repository: 'github://sbragagnolo/Fog/src'; load. "
+./pharo Pharo eval --save " Metacello new baseline: 'Enma'; repository: 'github://sbragagnolo/Enma/src'; load. "
 cd ..
 cp ../*.sh .
 cp ../*.st .
