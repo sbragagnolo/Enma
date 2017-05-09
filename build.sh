470de5defe70b28ba155df649eca16d745c47baf
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2017-05-09 17:47:41 +0200

    Adds iceberg to the build script

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 
 mkdir build/files -p
 cd build/files 
-wget -O- get.pharo.org/50+vm | bash
-./pharo Pharo eval --save " Metacello new baseline: 'Enma'; repository: 'github://sbragagnolo/Enma/src'; load. "
+wget -O- get.pharo.org/60+vmT | bash
+./pharo Pharo eval --save " Metacello new baseline: 'Iceberg'; repository: 'github://npasserini/iceberg';load. Metacello new baseline: 'Enma'; repository: 'github://sbragagnolo/Enma/src'; load. "
 cd ..
 cp ../*.sh .
 cp ../*.st .
